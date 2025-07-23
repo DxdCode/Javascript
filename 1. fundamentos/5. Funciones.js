b47d5f31208b3ca3ace2f2cca8b48c570ae6a57a
Stalin David <davidmuela2407@gmail.com>
2025-07-22 23:55:53 -0500

    Continuando

--- 1. fundamentos/5. Funciones.js
+++ 1. fundamentos/5. Funciones.js
@@ -1,5 +1,5 @@
 // FUNCIONESS
-
+// Cuando necesitas una función reutilizable, definida en cualquier lugar, y quieres aprovecharla
 // funciones declarativas 
 function saludar(nombre) {
   console.log('Hola ' + nombre)
@@ -7,13 +7,13 @@ function saludar(nombre) {
 saludar('David')
 
 // funciones expresadas 
-
+// Cuando quieres guardar una función en una variable 
 const sumar = function(a, b) {
   return a + b
 }
 console.log(sumar(3, 4))
 
 // funcion flechas 
-
+// Cuando necesitas una función corta 
 const multiplicar = (a, b) => a * b
 console.log(multiplicar(2, 5))
--- 1. fundamentos/Ejercicios/ejercicio5.js
+++ 1. fundamentos/Ejercicios/ejercicio5.js
@@ -16,6 +16,23 @@ calcularAreaRectangulo('dsds',20)
 // 2️⃣ Función expresada esNumeroPrimo(num)
 // Determina si un número es primo (retorna true o false). Usa bucles y condiciones.
 
+
+function esNumeroPrimo(num) {
+    if (num <= 1) {
+        console.log("Numero negativo o 1 no son primos")
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            console.log("No es numero primo")
+            return false; 
+        }
+    }
+    console.log("Es numero primo")
+    return true;
+
+}
+esNumeroPrimo(3)
 // 3️⃣ Función flecha convertirTemperatura(valor, unidad)
 // Convierte temperatura entre Celsius y Fahrenheit.
 //     Si unidad es "C", convierte a Fahrenheit.
@@ -26,17 +43,65 @@ calcularAreaRectangulo('dsds',20)
 // 4️⃣ Función declarada clasificarNumero(num)
 // Retorna "Positivo", "Negativo" o "Cero" según el número. Usa if/else.
 
+function clasificarNumero(num){
+    if(num > 0){
+        return "Positivo"
+    }else if(num < 0){
+        return "Negativo"
+    }else{
+        return "Cero"
+    }
+}
+console.log(clasificarNumero(0))
+
 // 5️⃣ Función expresada sumarImpares(arr)
 // Recibe un array de números y retorna la suma solo de los números impares.
 
+const numeros = [1,3,5,2,57,7,12,51,64,12,17]
+function sumarImpares(arr){
+    let sumar = 0
+    for (let i = 0;i < arr.length;i++){
+        if( arr[i]%2 === 1){
+            sumar+=arr[i]
+        }
+    
+    }
+    return sumar
+}
+console.log(sumarImpares(numeros))
 // 6️⃣ Función flecha mayorDeTres(a, b, c)
 // Retorna el mayor entre tres números. Usa operadores y condiciones.
+
+const mayorDeTres = (a,b,c) => { 
+    if (a >= b && a >= c) {return a } else if (b >= a &&  b >= c ) {return b} else {return c} 
+}
+console.log(mayorDeTres(4,5,8))
 
 // 7️⃣ Función declarada calcularDescuento(precio, descuento)
 // Calcula y retorna el precio final luego de aplicar un descuento en porcentaje (ej: 20%).
 
+function calcularDescuento(precio,descuento){
+    return precio - ((descuento * precio)/100)
+}
+console.log(calcularDescuento(10,20))
+
+
 // 8️⃣ Función expresada contarVocales(texto)
 // Cuenta y retorna cuántas vocales (a, e, i, o, u) tiene un texto (ignore mayúsculas).
+
+const parrafo = 'Hola que tal'
+
+function contarVocales(texto){
+    vocales = ['a','e','i','o','u']
+    let total = 0
+    for (let caracter of texto){
+        vocales == caracter
+        total+=1
+
+    }
+}
+
+console.log(contarVocales(parrafo))
 
 // 9️⃣ Función flecha repetirTexto(texto, veces)
 // Retorna un string que repite el texto la cantidad de veces indicada.
